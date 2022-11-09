4495273d72e673108877fd04928b4d160272d364
sompooh <som.pooh.som.pooh@gmail.com>
2022-11-09 11:16:55 +0900

    불필요코드 삭제

--- Workspace.swift
+++ Workspace.swift
@@ -7,10 +7,6 @@
 
 import ProjectDescription
 
-//let appName = Environment.appName.getString(default: "MainApp")
-//let appNameIsMainApp = appName == "MainApp"
-//appNameIsMainApp ? ["Projects/MainApp", "Projects/DataApp"] : []
-
 let projects: [Path] = ["Projects/MusicPlayApp", "Projects/DataApp", "Projects/DomainApp", "Projects/MPPlayer"]
 
 let workspace = Workspace(name: "Development",
